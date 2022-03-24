069d8e06fb2637a72782419c165c4096be4c096e
Lu Htoo Kyaw <luhtookyaw98@gmail.com>
2022-03-24 21:34:01 +0630

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import {useRef, useState} from 'react';
+import "@tensorflow/tfjs-backend-webgl";
+import * as facemesh from "@tensorflow-models/face-landmarks-detection";
+import Webcam from 'react-webcam';
+import { drawMesh } from './utils';
 
 function App() {
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+  const [openCam, setOpenCam] = useState(false);
+
+  const runFacemesh = async() => {
+    setOpenCam(!openCam);
+
+    const net = await facemesh.load(facemesh.SupportedPackages.mediapipeFacemesh);
+
+    setInterval(() => {
+      detect(net);
+    }, 1);
+  }
+
+  const detect = async(net) => {
+    if (typeof webcamRef.current !== "undefined" && webcamRef.current !== null && webcamRef.current.video.readyState === 4) {
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.video.videoWidth;
+      const videoHeight = webcamRef.current.video.videoHeight;
+
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      const face = await net.estimateFaces({
+        input: video
+      });
+
+      console.log(face);
+
+      const ctx = canvasRef.current.getContext("2d");
+
+      requestAnimationFrame(()=>{
+        drawMesh(face, ctx)
+      });
+    }
+  }
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {openCam && <Webcam className={`cam ${openCam && 'cam-opened'}`} ref={webcamRef}/>}
+      <canvas className={`cam ${openCam && 'cam-opened'}`} onClick={runFacemesh} ref={canvasRef}/>
     </div>
   );
 }
